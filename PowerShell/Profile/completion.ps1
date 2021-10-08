dd5e55427960a91b52502c407489e6d25ff458be
Jimmy Briggs <jimmy.briggs@pwc.com>
2021-10-07 22:06:39 -0400

    feat: add git-cliff to completion

--- PowerShell/Profile/completion.ps1
+++ PowerShell/Profile/completion.ps1
@@ -1,5 +1,20 @@
+$psdir = (Split-Path -parent $profile)
+
 # Chocolatey
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+# Winget
+
+# Scoop
+
+# Docker
+
+# npm
+
+# git
+
+# Git Cliff
+. $psdir\Profile\completions\git-cliff.ps1
