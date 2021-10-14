cbeeb09578ba36c541b4789377bdf1d8f5915ea8
Jimmy Briggs <jimmy.briggs@pwc.com>
2021-10-14 01:14:00 -0400

    bug: fix issues with Update-ProfileModules working directory

--- PowerShell/Scripts/Update-ProfileModules.ps1
+++ PowerShell/Scripts/Update-ProfileModules.ps1
@@ -5,12 +5,15 @@ Function Update-ProfileModules {
     $mods = (Get-ChildItem $modpath -Directory).Name
     ConvertTo-Yaml -Data $mods -OutFile $ymlpath -Force
 
+    Set-Location "$HOME\Documents\PowerShell"
     git pull
-    git add modules/**
-    git commit -m "config: Updated modules configurations"
+    git add Modules/**
+    git commit -m "config: Updated module configuration"
+    Set-Location "$HOME\Documents"
     git-cliff -o "$HOME\Documents\CHANGELOG.md"
     git commit -m "doc: update CHANGELOG.md for added modules"
     git push
 
 }
 
+Update-ProfileModules
