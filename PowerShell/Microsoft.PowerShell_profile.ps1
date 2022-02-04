37ce2ec90893793e28e00c203417af7b1841adcb
Jimmy Briggs <jimmy.briggs@pwc.com>
2022-02-03 22:46:26 -0500

    feat: update profiles

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,14 +3,3 @@
 # -------------------------------------------------------
 # Current User, Current Host Powershell Core v7 $PROFILE:
 # -------------------------------------------------------
-
-# Load Functions, Aliases, and Completion
-$psdir = (Split-Path -parent $profile)
-
-If (Test-Path "$psdir\Profile\functions.ps1") { . "$psdir\Profile\functions.ps1" }
-If (Test-Path "$psdir\Profile\aliases.ps1") { . "$psdir\Profile\aliases.ps1" }
-If (Test-Path "$psdir\Profile\completion.ps1") { . "$psdir\Profile\completion.ps1" }
-If (Test-Path "$psdir\Profile\modules.ps1") { . "$psdir\Profile\modules.ps1" }
-
-Set-PSReadLineOption -PredictionSource History -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-Set-PSReadLineOption -PredictionViewStyle ListView -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -8,6 +8,17 @@
 $galleryinfo = Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" }
 if (-not($galleryinfo.InstallationPolicy.Equals("Trusted"))) { Set-PSRepository -Name PSGallery -InstallationPolicy Trusted }
 
+# Load Functions, Aliases, and Completion
+$psdir = (Split-Path -parent $profile)
+
+If (Test-Path "$psdir\Profile\functions.ps1") { . "$psdir\Profile\functions.ps1" }
+If (Test-Path "$psdir\Profile\aliases.ps1") { . "$psdir\Profile\aliases.ps1" }
+If (Test-Path "$psdir\Profile\completion.ps1") { . "$psdir\Profile\completion.ps1" }
+If (Test-Path "$psdir\Profile\modules.ps1") { . "$psdir\Profile\modules.ps1" }
+
+Set-PSReadLineOption -PredictionSource History -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+Set-PSReadLineOption -PredictionViewStyle ListView -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+
 # Prompt
 Set-PoshPrompt -Theme wopian -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
 
