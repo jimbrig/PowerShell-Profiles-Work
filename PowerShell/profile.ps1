0b2acbe44e2927b0cbba53eaa6823d198d729ed0
Jimmy Briggs <jimmy.briggs@pwc.com>
2022-09-12 19:52:24 -0400

    feat: add Network TLS 1.2 protocol to profile

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -4,7 +4,11 @@
 # Current User, All Hosts Powershell Core v7 $PROFILE:
 # ----------------------------------------------------
 
+# Set System to use TLS 1.2 for .NET:
+If ([Net.ServicePointManager]::SecurityProtocol -ne 'Tls12') {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+}
+
+# Loop through and dot-source `Profile` files:
 $psfiles = Join-Path (Split-Path -Parent $profile) "Profile" | Get-ChildItem -Filter "*.ps1"
 ForEach ($file in $psfiles) { . $file }
-
-
